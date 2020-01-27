7564987a245ac7bbda5e9e28bc48422b002b02d5
loading <49134864+load1n9@users.noreply.github.com>
2020-01-27 16:22:46 -0500

    Update game thingy.js

--- game thingy.js
+++ game thingy.js
@@ -31,25 +31,25 @@ function fade_in2() {
 function potion255() {
     var x = document.getElementById("potion25");
     if (x.innerHTML === "Heal 25 (5)") {
-        x.outerHTML = `<button onclick="potion254()" id="potion25">Heal 25 (4)</button>`;
+        x.innerHTML = `<button onclick="potion254()" id="potion25">Heal 25 (4)</button>`;
     }
 }
 function potion254() {
     var x = document.getElementById("potion25");
     if (x.innerHTML === "Heal 25 (4)") {
-        x.outerHTML = `<button onclick="potion253()" id="potion25">Heal 25 (3)</button>`;
+        x.innerHTML = `<button onclick="potion253()" id="potion25">Heal 25 (3)</button>`;
     }
 }
 function potion253() {
     var x = document.getElementById("potion25");
     if (x.innerHTML === "Heal 25 (3)") {
-        x.outerHTML = `<button onclick="potion252()" id="potion25">Heal 25 (2)</button>`;
+        x.innerHTML = `<button onclick="potion252()" id="potion25">Heal 25 (2)</button>`;
     }
 }
 function potion252() {
     var x = document.getElementById("potion25");
     if (x.innerHTML === "Heal 25 (2)") {
-        x.outerHTML = `<button onclick="potion251()" id="potion25">Heal 25 (1)</button>`;
+        x.innerHTML = `<button onclick="potion251()" id="potion25">Heal 25 (1)</button>`;
     }
 }
 function potion251() {
@@ -60,13 +60,13 @@ function potion251() {
 }
 
 var x = document.getElementById("enemy");
-var xx = x.innerHTML
-var xxx = x.innerHTML - 10
+var xx = x.innerHTML;
+var xxx = x.innerHTML - 10;
 var y = document.getElementById("player");
-var yy = y.innerHTML
-var yyy = y.innerHTML - 15
+var yy = y.innerHTML;
+var yyy = y.innerHTML - 15;
 var z = document.getElementById("enemyAttacked");
-var zz = z.innerHTML
-var ba = document.getElementById("button")
+var zz = z.innerHTML;
+var ba = document.getElementById("button");
 var attackEnemy = Math.floor(Math.random() * (xx - xxx + 1) + xxx);
 var enemyAttack = Math.floor(Math.random() * (yy - yyy + 1) + yyy);
